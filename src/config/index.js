d070a6222377aa595311358de742e5f6dc5af740
Wilson <double_j918@hotmail.com>
2022-05-30 11:34:55 +0800

    added environment

---
+++ src/config/index.js
@@ -0,0 +1,22 @@
+
+//This would contain some configuration details 
+
+const env = process.env.REACT_APP_ENV;
+//environment variables are variables usesd by the application itself
+// details such as secret keys/environtment are part of the envt variables
+// In react, environment variables start with REACT_APP_
+
+let config = {
+    baseURL: "https://localhost"
+}
+
+switch (env.toUpperCase()) {
+    case "STAGE":
+        configure.baseURL = "https://stage.skillsunion.com"
+        break;
+    case "PRODUCTION":
+        configure.baseURL = "https://skillsunion.com"
+        break;
+}
+
+export default config;
